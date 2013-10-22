aa00d1843dc29aa790fd5d2b5770f27d66de108b
Martin Downton <git@martindownton.eu>
2013-10-22 21:11:50 +0100

    Moved from member template to memberS, added user data in JSON format

--- pomodoro.js
+++ pomodoro.js
@@ -11,10 +11,9 @@ if (Meteor.isClient) {
 				console.log("You pressed the button");
 		}
 	});
-	*/
 
 	Template.member.username = function() {
-		return "Sterling";
+		return "!Sterling!";
 	}
 	Template.member.userimage = function() {
 		return "http://images1.wikia.nocookie.net/__cb20100221034242/archer/images/d/d5/Sterling.jpg"
@@ -34,6 +33,75 @@ if (Meteor.isClient) {
 	Template.member.extraclass = function() {
 		return " break owner"
 	};
+	*/
+
+	Template.members.membercollection = function() {
+		members = [
+			{
+				username: 	"Sterling",
+				userimage: 	"http://images1.wikia.nocookie.net/__cb20100221034242/archer/images/d/d5/Sterling.jpg",
+				time: 		"25:00",
+				status: 	"Waiting to Start",
+				controls: 	[
+					{action: "ctl_start_pomodori", content: "start pomodori"}
+				],
+				extraclass:	""
+			},
+			{
+				username:	"Lana",
+				userimage:	"http://images4.wikia.nocookie.net/__cb20110213005818/archer/images/thumb/f/f3/Lana.png/250px-Lana.png",
+				time: 		"23:45",
+				status: 	"Busy Working Hard",
+				controls: 	[
+					{action: "ctl_exit", content: "exit"}
+				],
+				extraclass:	"busy owner"
+			},
+			{
+				username:	"Malory",
+				userimage:	"http://images3.wikia.nocookie.net/__cb20100418103125/archer/images/thumb/5/5d/Maloryarcher.jpg/233px-Maloryarcher.jpg",
+				time:		"25:00",
+				status:		"Finished Pomodori",
+				controls:	[
+					{action: "ctl_start_break", content: "start break"},
+					{action: "ctl_exit", content: "exit"}
+				],
+				extraclass:	"complete"
+			},
+			{
+				username:	"Cheryl",
+				userimage:	"http://images4.wikia.nocookie.net/__cb20110213005944/archer/images/thumb/b/b3/Cheryl.png/233px-Cheryl.png",
+				time:		"04:56",
+				status:		"On a Break",
+				controls:	[
+					{action: "ctl_exit", content: "exit"}
+				],
+				extraclass:	"break"
+			},
+			{
+				username:	"Pam",
+				userimage:	"http://images2.wikia.nocookie.net/__cb20110213010123/archer/images/thumb/d/da/Pam.png/233px-Pam.png",
+				time:		"05:00",
+				status:		"Finished Break",
+				controls:	[
+					{action: "ctl_start_pomodori", content: "start pomodori"}
+				],
+				extraclass:	"completebreak"
+			},
+			{
+				username:	"Krieger",
+				userimage:	"http://images3.wikia.nocookie.net/__cb20110213005457/archer/images/thumb/f/fc/Krieger.png/233px-Krieger.png",
+				time:		"12:34",
+				status:		"Busy Working Hard",
+				controls:	[
+					{action: "ctl_alert", content: "alert me when complete"}
+				],
+				extraclass:	"busy"
+			}
+		];
+
+		return members;
+	}
 }
 
 if (Meteor.isServer) {
