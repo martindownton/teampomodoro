2103dd771fc5849222398ea48d79c426b7fafd18
Martin Downton <git@martindownton.eu>
2013-10-24 12:03:41 +0100

    Added click controls to controls template

--- pomodoro.js
+++ pomodoro.js
@@ -12,27 +12,6 @@ if (Meteor.isClient) {
 		}
 	});
 
-	Template.member.username = function() {
-		return "!Sterling!";
-	}
-	Template.member.userimage = function() {
-		return "http://images1.wikia.nocookie.net/__cb20100221034242/archer/images/d/d5/Sterling.jpg"
-	};
-	Template.member.time = function() {
-		return "25:00"
-	};
-	Template.member.status = function() {
-		return "Waiting to Start"
-	};
-	Template.member.controls = function() {
-		return [
-			{action: "ctl_start_break", content: "start break"},
-			{action: "ctl_exit", content: "exit"}
-		]
-	};
-	Template.member.extraclass = function() {
-		return " break owner"
-	};
 	*/
 
 	Template.members.membercollection = function() {
@@ -102,6 +81,14 @@ if (Meteor.isClient) {
 
 		return members;
 	}
+
+	Template.controls.events({
+	'click a' : function (event) {
+		// template data, if any, is available in 'this'
+			if (typeof console !== 'undefined')
+				console.log(event)
+		}
+	});
 }
 
 if (Meteor.isServer) {
