31dd42d6759ee6f3661e68c34db9b0f69e587766
Martin Downton <git@martindownton.eu>
2014-06-14 18:53:44 +0100

    Added autoprefixer to Gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,11 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
+var apf = require('gulp-autoprefixer');
 
-gulp.task('sass', function() {
+gulp.task('css', function() {
 	gulp.src('./app/client/templates/sass/*.scss')
 		.pipe(sass())
+		.pipe(apf('last 2 version', 'ie 8', 'ie 9'))
 		.pipe(gulp.dest('./app/client/templates/css'));
 });
 
@@ -11,7 +13,7 @@ gulp.task('watch', function() {
     gulp.watch([
     	'./app/client/templates/sass/*.scss',
     	'./app/client/templates/sass/modules/*.scss',
-    ], ['sass']);
+    ], ['css']);
 });
 
-gulp.task('default', ['sass']);
+gulp.task('default', ['css']);
